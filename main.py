8f0928550fa42bb03e1ddc495edfbcaf45a732b6
Randall Klein <randall.klein@linux.com>
2026-07-27 23:15:11 -0500

    s

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ async def main():
         Xylan = account.get_character("Xylan") or next(iter(account.characters.values()))
         for count in range(1000):
             while Xylan.is_inventory_full == False:
-                await Xylan.rest()
+                await Xylan.actions.rest()
                 try:
-                    await Xylan.fight(target="chicken")
+                    await Xylan.actions.fight(target="chicken")
                 except InventoryFullError:
                     break
-            await Xylan.deposit_all()
-            await Xylan.deposit_gold(Xylan.gold)
+            await Xylan.actions.deposit_all()
+            await Xylan.actions.deposit_gold(Xylan.gold)
 
         return account, db, api
 
